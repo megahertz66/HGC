26b63935cda8c2d8cea80bf1f93564d88fb643ac
megahertz66 <38521608+megahertz66@users.noreply.github.com>
2019-09-11 11:03:03 +0800

    conflict

--- xinxin_videos/getVideos.py
+++ xinxin_videos/getVideos.py
@@ -1,5 +1,21 @@
+import re
+import os
 
-
-# 解析下载网址
+# 解析下载网址,返回可用的下载网址
 def getWebAddress(fileName):
+    webArray=[]
     file = open(fileName)
+
+    while True:
+        fileInLine = file.readline()
+        if not fileInLine:
+            break
+        fileUrl = re.findall(r'^http\S+', str(fileInLine))
+        if(len(fileUrl)):
+            webArray.append(fileUrl)
+    return webArray
+
+
+# 进入目录
+def getFileInDir(dirName):
+    os.listdir(os.getcwd)
