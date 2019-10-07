6e61d0c9c3f06b87858d2f789d53b9dd3ef115ac
Hert Zhao <zhao13836892152@foxmail.com>
2019-10-07 22:00:16 +0800

    添加笔记

---
+++ others/check_round/main.py
@@ -0,0 +1,23 @@
+import ctypes
+import win32api,win32gui,win32con
+from ctypes import *
+import time
+import math
+
+# def clickLefCur():
+#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN|win32con.MOUSEEVENTF_MIDDLEUP, 0, 0)
+#
+# def moveCurPos(x, y):
+#     windll.user32.SetCursorPos(x, y)
+#
+# def getCurPos():
+#     return win32gui.GetCursorPos()
+
+print(math.cos(math.pi/6))
+
+
+
+
+
+
+
